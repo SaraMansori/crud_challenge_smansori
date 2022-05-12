1f595e365663b9feedfe743c911d974975285ce6
Sara Mansori <smansori@uoc.edu>
2022-05-12 20:02:29 +0200

    add: movieServices

---
+++ src/services/moviesService.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { IData } from "../types";
+
+const moviesService = axios.create({
+  baseURL: 'http://localhost:5000/films',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export function getAllMovies() {
+  return moviesService.get('/')
+}
+
+export function getOneMovie(id: string) {
+  return moviesService.get(`/${id}`)
+}
+
+export function createMovie({ title, releaseDate, runningTime, director, producer, score }: IData) {
+  const newMovie = {
+    title,
+    release_date: releaseDate,
+    running_time: runningTime,
+    director,
+    producer,
+    rt_score: score,
+  }
+  return moviesService.post(`/`, newMovie)
+}
+
+export function editOneMovie(id: string, { title, releaseDate, runningTime, director, producer, score }: IData) {
+  const editedMovie = {
+    title,
+    release_date: releaseDate,
+    running_time: runningTime,
+    director,
+    producer,
+    rt_score: score,
+  }
+  return moviesService.put(`/${id}`, editedMovie)
+}
+
+export function deleteOneMovie(id: string) {
+  return moviesService.delete(`/${id}`)
+}
