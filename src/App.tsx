6f5e518f54480fb7b5e850c648cc531d140f8c5f
Sara Mansori <smansori@uoc.edu>
2022-05-12 20:05:10 +0200

    add: MainPage components and basic files update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import MainPage from "./pages/MoviesList";
+import { TableContextProvider } from "./context/Table/TableContext";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Studio Ghibli Movies</h1>
       </header>
+      <TableContextProvider>
+        <main>
+          <MainPage />
+        </main>
+      </TableContextProvider>
     </div>
   );
 }
---
+++ src/pages/MoviesList.tsx
@@ -0,0 +1,134 @@
+import { useState, useEffect, useMemo } from 'react';
+import { useFetch } from '../hooks/useFetch';
+
+import { IData, ITableOptions } from '../types';
+
+import CustomTable from '../components/Table/CustomTable';
+import TableHeader from '../components/Table/TableHeader/TableHeader';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import {
+  getAllMovies,
+  createMovie,
+  editOneMovie,
+  deleteOneMovie
+} from '../services/moviesService';
+
+import { parseAPIData } from '../utils';
+
+function MoviesList() {
+
+  const [movies, setMovies] = useState<IData[]>([]);
+
+  const [tableOptions, setTableOptions] = useState<ITableOptions>({
+    text: '',
+    sortBy: '',
+    sortOrder: '',
+    columnKeys: []
+  })
+
+  const newFilteredMovies = useMemo(() => {
+    return movies.filter(element =>
+      Object.values(element).some(property =>
+        property.toLowerCase().includes(tableOptions.text)
+      )
+    )
+  }, [movies, tableOptions.text])
+
+  const handleSearchBarTextChange = (value: string) => {
+    setTableOptions({ ...tableOptions, text: value });
+  };
+
+  const handleMovieSubmit = (newMovie: IData) => {
+    createMovie(newMovie)
+      .then(({ data }) => console.log(data))
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  const handleMovieEdit = (editedFilm: IData) => {
+    editedFilm.id &&
+      editOneMovie(editedFilm.id, editedFilm)
+        .then(({ data }) => {
+          const indexOfMovie = [...movies].findIndex((film) => film.id === data.id)
+          setMovies((prevState) => {
+            const moviesCopy = [...prevState]
+            moviesCopy.splice(indexOfMovie, 1, parseAPIData([data])[0])
+            return moviesCopy
+          })
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+  };
+
+  const handleMovieDelete = (movieToDelete: IData) => {
+    movieToDelete.id &&
+      deleteOneMovie(movieToDelete.id)
+        .then(({ data }) => {
+          const indexOfMovie = [...movies].findIndex((film) => film.id === data.id)
+          setMovies((prevState) => {
+            const moviesCopy = [...prevState]
+            moviesCopy.splice(indexOfMovie, 1)
+            return moviesCopy
+          })
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+  }
+
+  const { data, error, loading } = useFetch(getAllMovies)
+
+  useEffect(() => {
+
+    if (data) {
+      const formattedData = parseAPIData(data)
+      setMovies(formattedData);
+      if (formattedData.length > 0) {
+        setTableOptions({
+          ...tableOptions, columnKeys: Object.keys(formattedData[0])
+        })
+      }
+    }
+
+  }, [data])
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center" fluid>
+        <Col>
+          {tableOptions.columnKeys.length > 0 && (
+            <>
+              <TableHeader
+                handleMovieSubmit={handleMovieSubmit}
+                handleSearchBarTextChange={handleSearchBarTextChange}
+                tableOptions={tableOptions}
+              />
+              {loading ?
+
+                <p>Loading...</p>
+
+                :
+
+                <CustomTable
+                  data={newFilteredMovies}
+                  columnKeys={tableOptions.columnKeys}
+                  sortBy={tableOptions.sortBy}
+                  sortOrder={tableOptions.sortOrder}
+                  setTableOptions={setTableOptions}
+                  handleMovieEdit={handleMovieEdit}
+                  handleMovieDelete={handleMovieDelete}
+                />
+              }
+            </>
+          )}
+        </Col>
+      </Row>
+    </Container>
+
+  );
+}
+
+export default MoviesList;
