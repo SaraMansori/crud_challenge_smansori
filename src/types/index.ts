0184a6ec3163098bfb9fa4b1caef91c0f2ba0d73
Sara Mansori <smansori@uoc.edu>
2022-05-12 20:03:51 +0200

    add: index in types

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { IData, ITableOptions } from "./ITable";
+import { IfetchedData } from "./IFetchedData";
+import { IIndexable } from "./IIndexable";
+
+export type { IData, IfetchedData, IIndexable, ITableOptions };
