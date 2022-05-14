1d86c31f26f0d979604e8a25417d1360ad52bd21
Sara Mansori <smansori@uoc.edu>
2022-05-14 10:57:28 +0200

    test: add CustomTable test

---
+++ src/components/Table/CustomTable.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'testing-library/jest-dom/extend-expect'
+import { render } from "@testing-library/react";
+
+import CustomTable from "./CustomTable";
+
+test('renders table', () => {
+  const table = {
+
+  }
+
+  const view = render(<CustomTable />)
+})
