a92be9212dd0f51fb70d182a5f9b46ffe66b0b95
Sara Mansori <smansori@uoc.edu>
2022-05-15 19:27:54 +0200

    update: types changes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-import { IData, ITableState } from "./ITable";
+import { IData, ITableContext } from "./ITable";
 import { IfetchedData } from "./IFetchedData";
 import { IIndexable } from "./IIndexable";
 import { ITableAction } from "./IActions";
 
-export type { IData, IfetchedData, IIndexable, ITableState, ITableAction };
+export type { IData, IfetchedData, IIndexable, ITableContext, ITableAction };
